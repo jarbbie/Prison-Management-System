4590e6c376969c4d2978e7fe4fe79e82db37f71b
GGital <sakukiheng@gmail.com>
2026-03-21 00:06:54 +0700

    First Draft

---
+++ PrisonerIncident/incident_documented_by_officer.sql
@@ -0,0 +1,31 @@
+create or replace function list_incidents_by_officer(
+ target_officer_id int
+) returns table (
+ "Incident ID" int, 
+ "Incident Datetime" date, 
+ "Description" text, 
+ "Location Name" text,
+ "Prisoner Code" text,
+ "Prisoner First Name" text,
+ "Prisoner Last Name" text
+) 
+language sql stable as
+$$
+select pi.id as "Incident ID",
+       pi.incident_datetime as "Incident Datetime",
+       pi.description as "Description",
+       pl.name as "Location Name",
+       p.code as "Prisoner Code",
+       pr.first_name as "Prisoner First Name",
+       pr.last_name as "Prisoner Last Name"
+from PrisonerIncidents pi
+join PrisonLocation pl on pi.prison_location_id = pl.id
+left join InvolvedPrisoner ip on pi.id = ip.prisoner_incicent_id
+left join Prisoner p on ip.prisoner_id = p.id
+left join Person pr on p.person_id = pr.id
+where pi.reporting_officer_id = target_officer_id
+order by pi.incident_datetime DESC;
+$$;
+
+-- Replace '4' with the actual Reporting Officer ID
+SELECT * from list_incidents_by_officer(4);
