35d64df7f17c2432c69717aab8f93a8b619f2d21
MeldyRose <125353842+MeldyRose@users.noreply.github.com>
2026-03-26 20:43:23 +0700

    Update report3_nurse_workload_percentage.sql

--- treatment/report3_nurse_workload_percentage.sql
+++ treatment/report3_nurse_workload_percentage.sql
@@ -1,6 +1,6 @@
---Show all nurse workloads in percentage
---For assigning nurse workload in the future
 --67070503432 Praewa Thuwatharanimitkul
+--Find all nurse workloads in percentage and select to show only top 5
+--For assigning nurse workload in the future
 SELECT pe.first_name as "Nurse First Name", 
 pe.last_name as "Nurse Last Name", 
 ROUND(COUNT(*) * 100 / SUM(COUNT(*)) OVER(),2) as "Nurse Workload Percentage" 
